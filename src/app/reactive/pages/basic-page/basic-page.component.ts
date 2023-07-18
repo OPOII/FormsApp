57cdaf555e0a1926bfd7f6edd367d4139b3d9ac7
OPOII <bryancamili198@hotmail.com>
2023-07-18 17:25:47 -0500

    métodos de ayuda para los errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -20,6 +20,31 @@ export class BasicPageComponent implements OnInit {
     this.myForm.reset(rtx5090);
 
   }
+
+  isValidField(field:string):boolean|null{
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+  getFieldError(field:string):string|null{
+
+    if(!this.myForm.controls[field])return null;
+
+    const errors=this.myForm.controls[field].errors||{};
+
+    for (const key of Object.keys(errors)) {
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+
+        case 'required':
+          return 'Este campo es requerido';
+      }
+    }
+    return null;
+  }
   public myForm:FormGroup=this.fb.group({
     name: ['',[Validators.required,Validators.minLength(3)]],
     price: [0,[Validators.required,Validators.min(0)]],
@@ -28,7 +53,10 @@ export class BasicPageComponent implements OnInit {
 
   onSave():void{
 
-    if(this.myForm.invalid)return;
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    };
     console.log(this.myForm.value)
     this.myForm.reset({price:0,inStorage:0});
   }
