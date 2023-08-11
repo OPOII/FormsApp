f5df2a9181ecf7df8b0214e7627b2ef86adc24ba
OPOII <bryancamili198@hotmail.com>
2023-08-11 18:03:52 -0500

    formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup=this.fb.group({
+    name: ['',[Validators.required]],
+    email: ['',[Validators.required]],
+    username: ['',[Validators.required]],
+    password: ['',[Validators.required,Validators.minLength(6)]],
+    password2: ['',[Validators.required,Validators.minLength(6)]],
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  isValidField(field:string){
+
+  }
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 }
